9df6eef2e6c8d0e9430197948c2d60f346ebe37c
Flowzeliak <38844285+flowzeliak@users.noreply.github.com>
2019-07-29 22:34:59 -0700

    Assignment 3

--- index.js
+++ index.js
@@ -1,42 +1,79 @@
-const http = require("http"); 
-const fs = require("fs");
+'use strict';
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+
+// const http = require("http"); 
+// const fs = require("fs");
 const cheez = require("./cheez.js");
-let qs = require("querystring");
-http.createServer((req,res) => {
-  const path = req.url.toLowerCase();
-  const url = req.url.split("?");
-  const query = qs.parse(url[1]);
-  const bumbum = url[0];
-  switch(bumbum) {
-    case '/':   
-      fs.readFile("public/home.html", (err, data) => {
-      if (err) return console.error(err);
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data.toString());
-   });
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About page');
-      break;
-    case '/getall':
-      res.end(JSON.stringify(cheez.getAll()));
-      break;
-    case '/get':
-      let burger = cheez.get(query.name);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      let results = (burger) ? JSON.stringify(burger) : "Not found";
-      res.end(results);
-      break;
-    case '/delete':
-      let ding = cheez.get(query.name);
-      cheez.dilly(query.name);
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('Item Deleted ' + ding.name);
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('Not found');
-      break;
-    }
-}).listen(process.env.PORT || 3000);
+// let qs = require("querystring");
+
+// http.createServer((req,res) => {
+//   const path = req.url.toLowerCase();
+//   const url = req.url.split("?");
+//   const query = qs.parse(url[1]);
+//   const bumbum = url[0];
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions  
+
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
+app.set("view engine", ".html");
+ 
+app.post('/detail', (req, res) => {
+  let search = req.body.name_field;
+  let result = cheez.get(req.body.name_field);
+  res.render('detail', {search: search, result: result }); 
+});
+
+app.post('/delete', (req, res) => {
+  let ding = req.body.name_field;
+  let result = cheez.get(req.body.name_field);
+  cheez.dilly(ding);
+  let cheezlength = cheez.getAll().length;
+  res.render('delete', {ding: ding, result: result, cheezlength: cheezlength }); 
+});
+
+app.get('/', (req, res) => {
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/home.html'); 
+ });
+ 
+app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('About Page');
+ });
+
+ app.get('/getall', (req, res) => {
+  res.type('text/plain');
+  res.send(JSON.stringify(cheez.getAll()));
+ });
+
+app.get('/get', (req, res) => {
+  let burger = cheez.get(req.query.name);
+  let results = (burger) ? JSON.stringify(burger) : "Not found";
+  res.send(results);
+ });
+
+app.get('/delete', (req, res) => {
+  let ding = cheez.get(req.query.name);
+  cheez.dilly(req.query.name);
+  res.send('Item Deleted ' + ding.name);
+ });
+
+app.use( (req,res) => {
+  res.type('text/plain'); 
+  res.status(404);
+  res.send('404 - Not found');
+ });
+
+ app.listen(app.get('port'), () => {
+  console.log('Express started'); 
+ });
+ 
+
+  //  }
+//}).listen(process.env.PORT || 3000);
